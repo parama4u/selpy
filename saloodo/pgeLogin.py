4bae80e3535441f6be4e0dc5b5c50845c50a73ba
Param <Parama4u@gmail.com>
2018-10-11 00:09:07 +0900

    updates

--- saloodo/pgeLogin.py
+++ saloodo/pgeLogin.py
@@ -1,5 +1,6 @@
 
 import selpy.selpy
+from selenium.webdriver.common.by import By
 
 class PageLogin:
 
@@ -8,9 +9,19 @@ class PageLogin:
 
     def __init__(self):
         self.obj=selpy.selpy.SelPy()
-        print("PageLogin")
-        self.obj.start_chrome("http://www.google.co.in")
 
+    def fldEmail(self):
+        return self.obj.find_element(By.NAME,'_email')
 
+    def fldPassWord(self):
+        retrun self.obj.find_element(By.NAME,'_password')
 
+    def btnLogin(self):
+        return self.obj.find_element(By.XPATH, '//button[text()="Log in"]')
+
+    def setEmail(self,txtEmail):
+        self.fldEmail.typeText(txtEmail)
+
+    def setPassWord(self,txtPassWord):
+        self.fldPassword.typeText(txtPassWord)
 
--- selpy/selpy.py
+++ selpy/selpy.py
@@ -6,13 +6,13 @@ class SelPy:
     driver=None
 
     def __init__(self):
-        pass
-
-    def start_chrome(self,url):
         try:
             dr=selenium.webdriver.Chrome()
             dr.get(url)
             self.driver=dr
+
         except:
             print("Error while starting chrome driver")
 
+    def navigate(self,url):
+        self.driver.get(url)
